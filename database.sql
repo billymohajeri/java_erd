7db317d08d98acdb097dad86fee51d0bb16266f8
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-08-01 17:43:44 +0300

    Add orders table

--- database.sql
+++ database.sql
@@ -8,3 +8,54 @@ CREATE TABLE users (
     phone VARCHAR(15),
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    date TIMESTAMP NOT NULL,
+    comments TEXT,
+    status VARCHAR(30) NOT NULL,
+    address VARCHAR(255)
+);
+
+CREATE TABLE products (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    description TEXT,
+    image VARCHAR(255),
+    color VARCHAR(50) NOT NULL,
+    meta JSON,
+    rating NUMERIC(3,2)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
