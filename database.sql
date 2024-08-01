347ba19977c583eb73bfc0be25bf60edd0d1264a
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-08-01 19:47:43 +0300

    Add wishlist table

--- database.sql
+++ database.sql
@@ -57,7 +57,12 @@ CREATE TABLE reviews (
     image VARCHAR(255)
 );
 
-
+CREATE TABLE wishlist (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    product_id UUID NOT NULL REFERENCES products(id),
+    name VARCHAR(50)
+);
 
 
 
